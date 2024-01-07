bbbac787359b309c3a7c604bfb6967f3508b709f
Samuel Adeniran <127601699+Godofmachine@users.noreply.github.com>
2024-01-07 13:03:25 +0100

    latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html', "./node_modules/flowbite/**/*.js"],
+  content: ["./build/*.html",  "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+
+    
+    extend: {fontFamily: {
+      'king-richard': ['King Richard', 'sans'],
+    },
+  },
+  fontWeight: {
+    'king-richard-bold': 'bold',
+  },
+  // ...},
   },
   corePlugins: {
     preflight: false,
